5218f704261cd6c27227ca32421b7ead9f77385b
reeli <reeli.null@gmail.com>
2022-08-08 18:23:15 +0800

    define custom node

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,42 +1,54 @@
-import ReactFlow from 'react-flow-renderer';
-import {FC, useState} from "react";
+import ReactFlow, {useNodesState, useEdgesState} from 'react-flow-renderer';
+import {FC} from "react";
 import {ReactFlowProps} from "react-flow-renderer/dist/esm/types/component-props";
 
-interface Props extends ReactFlowProps{
+interface Props extends ReactFlowProps {
 }
 
 const initialNodes = [
   {
     id: '1',
-    type: 'input',
-    data: { label: 'Input Node' },
-    position: { x: 250, y: 25 },
+    type: 'dragHandleNode',
+    data: {label: 'Input Node'},
+    position: {x: 250, y: 25},
   },
 
   {
     id: '2',
+    type: "dragHandleNode",
     // you can also pass a React component as a label
-    data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
+    data: {label: "Default NOde"},
+    position: {x: 100, y: 125},
   },
   {
     id: '3',
-    type: 'output',
-    data: { label: 'Output Node' },
-    position: { x: 250, y: 250 },
+    type: 'dragHandleNode',
+    data: {label: 'Output Node'},
+    position: {x: 250, y: 250},
   },
 ];
 
 const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
-  { id: 'e2-3', source: '2', target: '3', animated: true },
+  {id: 'e1-2', source: '1', target: '2'},
+  {id: 'e2-3', source: '2', target: '3', animated: true},
 ];
 
-export const Chart:FC<Props>= () =>{
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+export const Chart: FC<Props> = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
-  return <ReactFlow nodes={nodes} edges={edges} fitView   style={{
-    backgroundColor: '#D3D2E5',
-  }} />;
+  return (
+    <>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        style={{
+          backgroundColor: '#D3D2E5',
+        }}
+        fitView
+      />
+    </>
+  );
 }
